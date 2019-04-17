330f20594327255222b360f878a49ba3c8d92793
Florianwuerti <florian@wuertenberger-design.ch>
2019-04-18 00:26:34 +0200

    remove _header.blade

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -2,7 +2,7 @@
 <header id="masthead">
     <div class="navbar">
         <div class="branding">
-            <img class="branding__logo" src="{{asset('images/logo.svg')}}" alt="Agentur OnOff Design Styleguide">
+            <img class="branding__logo" src="{{asset('images/logo.svg')}}" alt="">
         </div>
         <!-- Main Nav start -->
     @include('partials._topnav')
